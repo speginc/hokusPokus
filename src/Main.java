6c04de2cc984453f7b4682be111517e6ffd76fd9
speginc <158029598+speginc@users.noreply.github.com>
2024-01-29 12:26:27 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helloworld!");//kk565
-        for(int i = 1; i<=10; i++)
+        for(int i = 1; i<=10; i++){}
             
 
 
